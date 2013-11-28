5707af7a2fdd3b1b82250c329d1791e9136da085
terut <terut.dev+github@gmail.com>
2013-11-29 01:25:32 +0900

    Add jenkins

---
+++ site-cookbooks/jenkins/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: jenkins
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'java' do
+  action :remove
+end
+
+package 'java-1.7.0-openjdk' do
+  action :install
+end
+
+bash 'add repos' do
+  code <<-EOC
+    wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+    rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+  EOC
+  creates '/etc/yum.repos.d/jenkins.repo'
+end
+
+package 'jenkins' do
+  action :install
+end
+
+service 'jenkins' do
+  action [ :enable, :start ]
+  supports start: true, stop: true, restart: true
+end
